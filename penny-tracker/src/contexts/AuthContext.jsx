a0592eae1f403a08e01c15a7edeabdbbbf658b20
PIPetkova19 <PIPetkova19@codingburgas.bg>
2025-09-04 13:29:22 +0300

    Save users to localeStorage and add myUsers table in supabase to fix signup

--- penny-tracker/src/contexts/AuthContext.jsx
+++ penny-tracker/src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 import { supabase } from "../supabase/supabase-client";
 import { useNavigate } from "react-router";
 
@@ -7,57 +7,141 @@ export const AuthContext = createContext();
 function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
     let navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        //function that checks if there is an active session on INITIAL load
+        const getSession = async () => {
+            setIsLoading(true);
+            const { data: { session } } = await supabase.auth.getSession();
+            //if there is no session the user is set to null
+            setUser(session?.user ?? null);
+            setIsLoading(false);
+        };
+        getSession();
+    }, []);
 
     async function handleSignUp(email, password) {
-        const { data, error } = await supabase.auth.signUp({ email, password })
-        if (error) { alert(error); throw error; }
-        alert("An email confirmation has been sent!");
-        setUser(data);
-        return data;
+        try {
+            setIsLoading(true);
+
+            const { data: existingUser, error: fetchError } = await supabase
+                .from('myUsers')
+                .select('email')
+                .eq('email', email)
+                .single();
+
+            if (existingUser) {
+                alert("User already registered!");
+                return null;
+            }
+            
+            else {
+                const { data, error } = await supabase.auth.signUp({ email, password });
+                if (error) {
+                    alert(error.message);
+                    throw error;
+                }
+
+                await supabase.from('myUsers').insert([{ email }]);
+                setUser(data.user);
+                alert("An email confirmation has been sent!");
+                return data.user;
+            }
+        } catch (error) {
+            alert(error.message);
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     async function handleSignIn(email, password) {
-        const { data, error } = await supabase.auth.signInWithPassword({ email, password })
-        if (error) { alert(error); throw error; }
-        setUser(data);
-        alert("Login succesfull!");
-        navigate("/");
+        try {
+            setIsLoading(true);
+            const { data, error } = await supabase.auth.signInWithPassword({ email, password })
+            if (error) { alert(error); throw error; }
+            alert("Login succesfull!");
+            setUser(data);
+            navigate("/");
+        }
+        catch (error) {
+            alert(error.message);
+        }
+        finally {
+            setIsLoading(false);
+        }
     }
 
     async function handleSignUpGoogle() {
-        const { data, error } = supabase.auth.signInWithOAuth({
-            provider: 'google',
-        })
-        if (error) { alert(error); throw error; }
-        setUser(data);
-        return data;
+        try {
+            setIsLoading(true);
+            const { data, error } = supabase.auth.signInWithOAuth({
+                provider: 'google',
+            })
+            if (error) { alert(error); throw error; }
+            setUser(data);
+            return data;
+        }
+        catch (error) {
+            alert(error.message);
+        }
+        finally {
+            setIsLoading(false);
+        }
     }
 
     async function handleSignOut() {
-        const { error } = supabase.auth.signOut();
-        if (error) { alert(error); throw error; }
-        setUser(null);
-        
+        try {
+            setIsLoading(true);
+            const { error } = supabase.auth.signOut();
+            if (error) { alert(error); throw error; }
+            setUser(null);
+        }
+        catch (error) {
+            alert(error.message);
+        }
+        finally {
+            setIsLoading(false);
+        }
     }
 
     //forgotten password
     async function handleResetPass(email) {
-        const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+        try {
+            setIsLoading(true);
+            const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
                 redirectTo: 'http://localhost:5173/updateUser',
-        })
-        alert("Email with link has been sent!");
-        if (error) { alert(error); throw error; }
+            })
+            alert("Email with link has been sent!");
+            if (error) { alert(error); throw error; }
+        }
+        catch (error) {
+            alert(error.message);
+        }
+        finally {
+            setIsLoading(false);
+        }
     }
 
     async function handleUpdateUser(password) {
-        const { data, error } = await supabase.auth.updateUser({ password })
-        if (error) { alert(error); throw error; }
-        setUser(data);
-        navigate("/login")
+        try {
+            setIsLoading(true);
+            const { data, error } = await supabase.auth.updateUser({ password })
+            if (error) { alert(error); throw error; }
+            setUser(data);
+            navigate("/login")
+        }
+        catch (error) {
+            alert(error); throw error;
+        }
+        finally {
+            setIsLoading(false);
+        }
     }
 
     return (
-        <AuthContext value={{ user, handleSignUp, handleSignIn, handleSignUpGoogle, handleSignOut ,
+        <AuthContext value={{
+            user, handleSignUp, handleSignIn, handleSignUpGoogle, handleSignOut,
             handleResetPass, handleUpdateUser
         }}>
             {children}
