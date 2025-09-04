171c894d92f9904bc35ea9fb072bc9da23702f2c
PIPetkova19 <PIPetkova19@codingburgas.bg>
2025-09-04 11:02:13 +0300

    Edit style of sign up

--- penny-tracker/src/registration/SignUp.jsx
+++ penny-tracker/src/registration/SignUp.jsx
@@ -11,9 +11,12 @@ import Google from "@mui/icons-material/Google";
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import InputAdornment from '@mui/material/InputAdornment';
 import LockIcon from '@mui/icons-material/Lock';
+import Box from '@mui/material/Box';
+import { useTheme } from "@mui/material/styles";
 
 function SignUp() {
   const { handleSignUp, handleSignUpGoogle } = use(AuthContext);
+  const theme = useTheme();
 
   const validationSchema = yup.object({
     email: yup
@@ -39,78 +42,141 @@ function SignUp() {
   });
 
   return (
-    <div>
-      <Typography variant="h4" component="h2">
-       Create an account
-      </Typography>
+    <div style={{ width: '100%', height: '80vh' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+          height: '100%'
+        }}>
 
- <Typography variant="subtitle1" component="h2">
-     Welcome to Penny Tracker. Register as a member.
-      </Typography>
+        <Box
+          sx={{
+            boxShadow: " rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
+            borderRadius: '8%',
+            width: { xs: '70%', sm: '50%', md: '40%', lg: '30%' },
+            minHeight: 'auto',
+            backgroundColor: theme.palette.background.paper
+          }}>
+          <form onSubmit={formik.handleSubmit}>
 
-      <form onSubmit={formik.handleSubmit}>
-        <TextField
-          slotProps={{
-            input: {
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              ),
-            },
-          }}
-          id="email"
-          name="email"
-          label="Email"
-          placeholder='your@email.com'
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <TextField
-            slotProps={{
-            input: {
-              startAdornment: (
-                <InputAdornment position="start">
-                  <LockIcon />
-                </InputAdornment>
-              ),
-            },
-          }}
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          placeholder='••••••'
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-        />
-        <Button color="primary" variant="contained" fullWidth type="submit"
-          style={{ textTransform: 'none' }}>
-         Sign Up
-        </Button>
+            <Box sx={{ pt: { xs: 3, sm: 4, md: 5, lg: 5 } }}>
+              <Typography variant="h4" component="h2">
+                Create an account
+              </Typography>
+            </Box>
 
-        <Typography variant="subtitle1" component="h2">
-          Already have an account?{' '}
-          <Link to="/login" style={{ textDecoration: 'none' }}>Log in</Link>
-        </Typography>
+            <Box>
+              <Typography variant="subtitle1" component="h2" sx={{
+                color: theme.palette.text.secondary
+              }}>
+                Welcome to Penny Tracker. Register as a member.
+              </Typography>
+            </Box>
 
-        <Divider>or</Divider>
+            <Box sx={{ pt: { xs: 3, sm: 4, md: 5, lg: 5 } }}>
+              <TextField
+                variant="filled"
+                slotProps={{
+                  input: {
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <AccountCircle />
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+                id="email"
+                name="email"
+                label="Email"
+                placeholder='your@email.com'
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+            </Box>
 
-        <Button
-          variant="outlined"
-          startIcon={<Google />}
-          style={{ textTransform: 'none' }}
-          onClick={handleSignUpGoogle}>
-          Sign up with Google
-        </Button>
+            <Box sx={{ pt: { xs: 1, sm: 2, md: 3, lg: 3 } }}>
+              <TextField
+                variant="filled"
+                slotProps={{
+                  input: {
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <LockIcon />
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                placeholder='••••••'
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+              />
+            </Box>
 
-      </form>
+            <Box sx={{ pt: { xs: 1, sm: 2, md: 3, lg: 3 } }}>
+              <Button color="primary" variant="contained" type="submit"
+                sx={{
+                  textTransform: 'none',
+                  pl: { xs: '70px', sm: '80px', md: '90px', lg: '100px' },
+                  pr: { xs: '70px', sm: '80px', md: '90px', lg: '100px' },
+                  backgroundColor: theme.palette.customColor
+                }}>
+                Sign Up
+              </Button>
+            </Box>
+
+            <Box sx={{ pt: { xs: 1, sm: 2, md: 3, lg: 3 } }}>
+              <Typography variant="subtitle1" component="h2"
+                sx={{ color: theme.palette.text.secondary }}>
+                Already have an account?{' '}
+                <Link to="/login"
+                  style={{
+                    textDecoration: 'none',
+                    color: theme.palette.customColor
+                  }}>
+                  Log in
+                </Link>
+              </Typography>
+            </Box>
+
+            <Box sx={{ pt: 2, ml: 5, mr: 5 }}>
+              <Divider sx={{ color: theme.palette.text.secondary }}>or</Divider>
+            </Box>
+
+            <Box
+              sx={{
+                pt: { xs: 1, sm: 2, md: 3, lg: 3 },
+                pb: { xs: 3, sm: 4, md: 5, lg: 5 }
+              }}>
+              <Button
+                variant="outlined"
+                startIcon={<Google />}
+                style={{
+                  textTransform: 'none',
+                  color: theme.palette.customColor,
+                  borderColor: theme.palette.customColor
+                }}
+                onClick={handleSignUpGoogle}>
+                Sign up with Google
+              </Button>
+            </Box>
+
+          </form>
+        </Box>
+      </Box>
     </div>
   );
 };
