871ebb1df7e2059a82a03493a0cd1132e8399689
PIPetkova19 <PIPetkova19@codingburgas.bg>
2026-01-15 16:04:19 +0200

    Change style for ResetPassword and UpdateUser

--- penny-tracker/src/registration/UpdateUser.jsx
+++ penny-tracker/src/registration/UpdateUser.jsx
@@ -3,16 +3,15 @@ import { use, useState } from 'react';
 import Button from "@mui/material/Button";
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { Divider } from '@mui/material';
-import Google from "@mui/icons-material/Google";
-import AccountCircle from '@mui/icons-material/AccountCircle';
 import InputAdornment from '@mui/material/InputAdornment';
 import LockIcon from '@mui/icons-material/Lock';
-import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
+import { useTheme } from "@mui/material/styles";
 
 function UpdateUser() {
     const {handleUpdateUser } = use(AuthContext);
     const [password, setPassword] = useState("");
+    const theme = useTheme();
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -20,12 +19,34 @@ function UpdateUser() {
         setPassword("");
     }
     return (
-        <>
-            <Typography variant="h4" component="h2">
-               Change your password
-            </Typography>
+        <div style={{ width: '100%', height: '100vh' }}>
+        <Box
+            sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center',
+            height: '100%'
+            }}>
+
+            <Box
+                      sx={{
+                        boxShadow: " rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
+                        borderRadius: '8%',
+                        width: { xs: '70%', sm: '50%', md: '40%', lg: '30%' },
+                        minHeight: 'auto',
+                        backgroundColor: theme.palette.background.paper
+                      }}>
+
+            <Box sx={{ pt: { xs: 3, sm: 4, md: 5, lg: 5 } }}>
+                <Typography variant="h4" component="h2">
+                Change your password
+                </Typography>
+           </Box>
 
             <form onSubmit={handleSubmit}>
+            <Box sx={{ pt: { xs: 3, sm: 4, md: 5, lg: 5 } }}>
                 <TextField
                     slotProps={{
                         input: {
@@ -42,11 +63,27 @@ function UpdateUser() {
                     placeholder='••••••'
                     onChange={(e) => setPassword(e.target.value)}
                 />
-                <Button color="primary" variant="contained" fullWidth type="submit">
+                </Box>
+
+                <Box
+              sx={{
+                pt: { xs: 1, sm: 2, md: 3, lg: 3 },
+                pb: { xs: 3, sm: 4, md: 5, lg: 5 }
+              }}>
+                 <Button color="primary" variant="contained" type="submit"
+                sx={{
+                  textTransform: 'none',
+                  pl: { xs: '70px', sm: '80px', md: '90px', lg: '100px' },
+                  pr: { xs: '70px', sm: '80px', md: '90px', lg: '100px' },
+                  backgroundColor: theme.palette.customColor
+                }}>
                     Submit
                 </Button>
+                </Box>
             </form>
-        </>
+            </Box>
+            </Box>
+        </div>
     )
 }
 
